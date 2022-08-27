b38b689e41c96cfa8866dfed356401f15710997d
nmy <nmykyoupuro@gmail.com>
2022-08-27 11:35:27 +0900

    手抜き

---
+++ front/app/pages/profile/profile-setting.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+type InputProps = {
+    type: string
+    size?: 'large' | 'small'
+    placeholder: string
+    labletext: string
+}
+
+const Input = ({type, size, placeholder, labletext}: InputProps)=> {
+    return (
+        <div className='my-4'>
+            <div className='text-lg'>{labletext}</div>
+            <input type={type} className={`h-12 ${size=='large'?'w-80':'w-40'} text-lg rounded bg-slate-200`} placeholder={placeholder}></input>
+        </div>
+    )
+}
+
+
+
+const Profile = () => {
+    return(
+        <div className='flex flex-col mx-auto my-12 w-80'>
+            <div className='text-2xl m-auto my-4'>プロフィールを入力しましょう</div>
+            <img src='/Profile.svg' className='w-20 my-2 mx-auto'></img>
+            <div className='my-2'>
+                <Input type='text' size='large' placeholder='入力してください' labletext='ニックネーム'></Input>
+                <Input type='text' size='large' placeholder='入力してください' labletext='生年月日'></Input>
+                <Input type='text' size='large' placeholder='入力してください' labletext='職業'></Input>
+            </div>
+            <div className='my-2'>
+                <div className='text-lg m-auto my-4'>基本情報</div>
+                <Input type='text' size='small' placeholder='入力してください' labletext='郵便番号'></Input>
+                <Input type='text' size='large' placeholder='入力してください' labletext='住所'></Input>
+                <Input type='text' size='large' placeholder='入力してください' labletext='番地'></Input>
+                <Input type='text' size='large' placeholder='入力してください' labletext='建物名'></Input>
+                <Input type='text' size='large' placeholder='入力してください' labletext='電話番号'></Input>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
